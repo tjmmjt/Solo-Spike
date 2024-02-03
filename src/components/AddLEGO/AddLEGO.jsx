9c6ffb4da4e60d15375df8d40b900bc4022c118c
Tucker Melssen <tuckerjmelssen@gmail.com>
2024-02-03 13:34:58 -0600

    Material UI install w/ fonts, icons

---
+++ src/components/AddLEGO/AddLEGO.jsx
@@ -0,0 +1,12 @@
+
+
+function AddLEGO(){
+    return(
+        <>
+            <h1>Hello!</h1>
+            
+        </>
+    )
+}
+
+export default AddLEGO
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import AddLEGO from '../AddLEGO/AddLEGO'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello!</h1>
+      <AddLEGO />
     </>
   )
 }
