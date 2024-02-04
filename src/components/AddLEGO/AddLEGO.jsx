f2bf33811bc69e5f7cd02f3f32cb5720d89d671e
Tucker Melssen <tuckerjmelssen@gmail.com>
2024-02-03 22:58:19 -0600

    resolved prior error by deleting node_modules/.vite/deps/. and relaunching dev

--- src/components/AddLEGO/AddLEGO.jsx
+++ src/components/AddLEGO/AddLEGO.jsx
@@ -1,80 +1,30 @@
-import * as React from 'react';
-import './AddLEGO.css'
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Box from '@mui/material/Box';
+import { Box, Grid, InputLabel, TextField, Typography } from "@mui/material";
 
-
-
-
-
-export default function PaymentForm() {
+function AddLEGO() {
   return (
     <Box
-        maxWidth={0.5}
-        sx={{
-            color:'white',
-            background: '#ffffff7e',
-            boxShadow: 3,
-            p: 2, 
-            border: '1px solid grey', 
-            borderRadius: 3
-            }}
+      component="section"
+      sx={{
+        boxShadow: 3,
+        p: 3,
+        border: "1px solid grey",
+        borderRadius: 3,
+        bgcolor: "white"
+      }}
     >
-      <Typography variant="h3" gutterBottom>
-        Add Lego
+      <Typography variant="h4" component="h2" fontWeight="bold">
+        ADD LEGO SET
       </Typography>
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={6}>
-          <TextField
-            required
-            id="cardName"
-            label="name your LEGO set"
-            fullWidth
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            required
-            id="cardNumber"
-            label="Card number"
-            fullWidth
-            autoComplete="cc-number"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            required
-            id="expDate"
-            label="Expiry date"
-            fullWidth
-            autoComplete="cc-exp"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            required
-            id="cvv"
-            label="CVV"
-            helperText="Last three digits on signature strip"
-            fullWidth
-            autoComplete="cc-csc"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <FormControlLabel
-            control={<Checkbox color="secondary" name="saveCard" value="yes" />}
-            label="Remember credit card details for next time"
-          />
-        </Grid>
-      </Grid>
+
+      <TextField
+          label="With normal TextField"
+          id="outlined-start-adornment"
+          sx={{ m: 1, width: '25ch' }}
+        />
+       
+
     </Box>
   );
 }
+
+export default AddLEGO;
