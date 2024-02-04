a4b21916b4d3cb51417bf4d24b6809c10aa3ba9d
Tucker Melssen <tuckerjmelssen@gmail.com>
2024-02-03 22:08:28 -0600

    getting console error --Uncaught TypeError: createTheme_default is not a function at ---  http://localhost:5173/node_modules/.vite/deps/chunk-OYRBKL5K.js\?v\=2fa28527

--- src/components/AddLEGO/AddLEGO.jsx
+++ src/components/AddLEGO/AddLEGO.jsx
@@ -1,12 +1,80 @@
+import * as React from 'react';
+import './AddLEGO.css'
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Box from '@mui/material/Box';
 
 
-function AddLEGO(){
-    return(
-        <>
-            <h1>Hello!</h1>
-            
-        </>
-    )
+
+
+
+export default function PaymentForm() {
+  return (
+    <Box
+        maxWidth={0.5}
+        sx={{
+            color:'white',
+            background: '#ffffff7e',
+            boxShadow: 3,
+            p: 2, 
+            border: '1px solid grey', 
+            borderRadius: 3
+            }}
+    >
+      <Typography variant="h3" gutterBottom>
+        Add Lego
+      </Typography>
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={6}>
+          <TextField
+            required
+            id="cardName"
+            label="name your LEGO set"
+            fullWidth
+            variant="standard"
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextField
+            required
+            id="cardNumber"
+            label="Card number"
+            fullWidth
+            autoComplete="cc-number"
+            variant="standard"
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextField
+            required
+            id="expDate"
+            label="Expiry date"
+            fullWidth
+            autoComplete="cc-exp"
+            variant="standard"
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextField
+            required
+            id="cvv"
+            label="CVV"
+            helperText="Last three digits on signature strip"
+            fullWidth
+            autoComplete="cc-csc"
+            variant="standard"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <FormControlLabel
+            control={<Checkbox color="secondary" name="saveCard" value="yes" />}
+            label="Remember credit card details for next time"
+          />
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
-
-export default AddLEGO
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,15 +4,20 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-
+import { Container } from '@mui/material';
+import { flexbox } from '@mui/system';
 import AddLEGO from '../AddLEGO/AddLEGO'
 
+
+
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <AddLEGO />
+      <Container sx={{display: 'flex', justifyContent: 'center'}}>
+        <AddLEGO />
+      </Container>
     </>
   )
 }
